00bd1eb1c0b4627c8d24ce5f771e254e0a1f5b8d
elpguillen <elpguillen@users.noreply.github.com>
2024-03-09 20:20:16 -0800

    Created WorkoutDao for inserting, updating, and deleting Workout objects.

---
+++ app/src/main/java/com/chiu/hiit_time/data/dao/WorkoutDao.kt
@@ -0,0 +1,22 @@
+package com.chiu.hiit_time.data.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import com.chiu.hiit_time.data.entities.Workout
+
+interface WorkoutDao {
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(workout: Workout)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(workouts: List<Workout>)
+    
+    @Update
+    suspend fun update(workout: Workout)
+
+    @Delete
+    suspend fun delete(workout: Workout)
+}
