0a6a193048853a32a9b45d23ef83941f5b106c5c
elpguillen <elpguillen@users.noreply.github.com>
2024-03-26 20:34:25 -0700

    Created the following tests in ExerciseDaoTest:   1. insertListExerciseToDB :        Checks that all exercises in a List<Exercise> are inserted into the         database using insert(exercises: List<Exercise>) given that all the         exercises in the list are different
      2. insertUniqueExercisesInListToDB :
           Checks that only unique exercises in a List<Exercise> are inserted
            into the database using insert(exercises: List<Exercise>)
    
      3. deleteExistingExerciseInDB :
           Checks that the delete function removes the given exercise if it exists
            in the database
    
      4. updateExistingExerciseInDB :
           Checks that the update function replaces the given exercise in the
            database

--- app/src/androidTest/java/com/chiu/hiit_time/ExerciseDaoTest.kt
+++ app/src/androidTest/java/com/chiu/hiit_time/ExerciseDaoTest.kt
@@ -58,4 +58,66 @@ class ExerciseDaoTest {
         Assert.assertEquals(allExercises.size, 1)
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun insertListExerciseToDB() = runBlocking {
+        val squatsExercise = Exercise(1, "squats", 10000, 5000, 2)
+        val pushupExercise = Exercise(2, "push-ups", 1, 1, 1)
+        val hiitExercise = Exercise(3, "hiit", 50000, 15000, 5)
+        val exercises = listOf(squatsExercise, pushupExercise, hiitExercise)
+        exerciseDao.insert(exercises)
+        val allExercises = exerciseDao.getAllExercises().first()
+
+        Assert.assertEquals(allExercises.size, exercises.size)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertUniqueExercisesInListToDB() = runBlocking {
+        val squatsExercise = Exercise(1, "squats", 10000, 5000, 2)
+        val pushupExercise = Exercise(2, "push-ups", 1, 1, 1)
+        val hiitExercise = Exercise(3, "hiit", 50000, 15000, 5)
+        val exercises = listOf(squatsExercise, squatsExercise, pushupExercise, pushupExercise, hiitExercise, hiitExercise)
+        val uniqueExercises = exercises.distinct()
+        exerciseDao.insert(exercises)
+        val allExercises = exerciseDao.getAllExercises().first()
+
+        Assert.assertEquals(allExercises.size, uniqueExercises.size)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun deleteExistingExerciseInDB() = runBlocking {
+        val squatsExercise = Exercise(1, "squats", 10000, 5000, 2)
+        exerciseDao.insert(squatsExercise)
+        val exercisesAfterInsert = exerciseDao.getAllExercises().first()
+        // The single exercise added should be in the database
+        Assert.assertEquals(exercisesAfterInsert.size, 1)
+        exerciseDao.delete(squatsExercise)
+        val exercisesAfterDelete = exerciseDao.getAllExercises().first()
+        // The only exercise previously added should have been deleted
+        // leaving no more exercises in the database
+        Assert.assertEquals(exercisesAfterDelete.size, 0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun updateExistingExerciseInDB() = runBlocking {
+        val squatsExercise = Exercise(1, "squats", 10000, 5000, 2)
+        val updatedSquatsExercise = Exercise(1, "squats", 1, 1, 2)
+        // the difference between squatsExercise and updatedSquatsExercise are there exerciseLength and restTime
+        // so will be checking they match with the corresponding database operation
+        exerciseDao.insert(squatsExercise)
+        val exerciseInserted = exerciseDao.getAllExercises().first()
+        Assert.assertEquals(exerciseInserted[0].id, 1)
+        Assert.assertEquals(exerciseInserted[0].exerciseLength, squatsExercise.exerciseLength)
+        Assert.assertEquals(exerciseInserted[0].restTime, squatsExercise.restTime)
+
+        exerciseDao.update(updatedSquatsExercise)
+        val exerciseUpdate = exerciseDao.getAllExercises().first()
+        Assert.assertEquals(exerciseUpdate[0].id, 1)
+        Assert.assertEquals(exerciseUpdate[0].exerciseLength, updatedSquatsExercise.exerciseLength)
+        Assert.assertEquals(exerciseUpdate[0].restTime, updatedSquatsExercise.restTime)
+    }
+
 }
