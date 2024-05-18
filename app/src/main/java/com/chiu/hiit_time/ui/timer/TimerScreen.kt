2df6777fa2b7e4621f7b56081d822d42ccb7b8d4
elpguillen <elpguillen@users.noreply.github.com>
2024-05-17 19:22:39 -0700

    Modified styling for the Timer in TimerScreen.
    Add implementation for the Timer:
      1. Timer starts right after navigating to TimerScreen
      2. Resume/Pause button starts/pauses timer.
      3. Restart button added but not implemented.

--- app/src/main/java/com/chiu/hiit_time/ui/timer/TimerScreen.kt
+++ app/src/main/java/com/chiu/hiit_time/ui/timer/TimerScreen.kt
@@ -1,5 +1,6 @@
 package com.chiu.hiit_time.ui.timer
 
+import android.os.CountDownTimer
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,15 +9,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.ReusableComposition
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.chiu.hiit_time.R
 import com.chiu.hiit_time.ui.AppViewModelProvider
 import com.chiu.hiit_time.ui.navigation.NavigationDestination
+import kotlinx.coroutines.delay
 
 object TimerDestination : NavigationDestination {
     override val route = "timer"
@@ -27,50 +37,53 @@ fun TimerScreen(
     modifier: Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Timer(time = 1000, modifier = modifier)
-        TimerOptions(onItemClick = { }, modifier = modifier)
+        CountDownTimer(120L)
     }
 }
 
-@Composable
-fun Timer(
-    time: Int,
-    modifier: Modifier
-) {
-    Text(text = time.toString())
-}
 
 @Composable
-fun TimerOptions(
-    onItemClick: () -> Unit,
-    modifier: Modifier
+fun CountDownTimer(
+    totalTimerTime: Long = 0L
 ) {
+    var timeLeft by remember {
+        mutableLongStateOf(totalTimerTime)
+    }
+
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(key1 = timeLeft, key2 = isTimerRunning) {
+        while (timeLeft > 0 && !isTimerRunning) {
+            delay(1000L)
+            timeLeft--
+        }
+    }
+
+    Text(
+        text = "$timeLeft",
+        fontSize = 96.sp,
+        modifier = Modifier.padding(top = 192.dp, bottom = 144.dp)
+    )
+
     Column(
-        modifier = Modifier
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { }) {
-            Text(text = "Workout Name")
-        }
-
-        Button(onClick = { }) {
-            Text(text = "Skip")
-        }
-
-        Row(
-            modifier = Modifier
+        Button(
+            onClick = { isTimerRunning = !isTimerRunning },
+            modifier = Modifier.padding(bottom = 16 .dp)
         ) {
-            Button(onClick = { }) {
-                Text(text = "Start/Pause")
-            }
+            Text(text = if (isTimerRunning) "Resume" else "Pause")
+        }
 
-            Button(onClick = { }) {
-                Text(text = "Restart")
-            }
+        Button(onClick = {}) {
+            Text(text = "Restart")
         }
     }
 }
@@ -80,10 +93,10 @@ fun TimerOptions(
 fun PreviewTimerScreen() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Timer(time = 1000, modifier = Modifier)
-        TimerOptions(onItemClick = { }, modifier = Modifier)
+        CountDownTimer()
     }
 }
