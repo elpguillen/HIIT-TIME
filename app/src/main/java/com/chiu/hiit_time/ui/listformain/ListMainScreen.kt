9292ac09b9a6df44e66b224eb7816f0d81791143
elpguillen <elpguillen@users.noreply.github.com>
2024-09-18 20:46:07 -0700

    Create MainScreen and ListMainScreen and corresponding composables.

---
+++ app/src/main/java/com/chiu/hiit_time/ui/listformain/ListMainScreen.kt
@@ -0,0 +1,12 @@
+package com.chiu.hiit_time.ui.listformain
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ListMainScreen(
+    modifier: Modifier
+) {
+
+}
---
+++ app/src/main/java/com/chiu/hiit_time/ui/main/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.chiu.hiit_time.ui.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(
+    modifier: Modifier
+) {
+
+}
