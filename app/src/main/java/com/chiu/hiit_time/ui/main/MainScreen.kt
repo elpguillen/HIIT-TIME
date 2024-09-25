d3fe31c53c02dbeef10ceb4f4fc4c39a95fad912
elpguillen <elpguillen@users.noreply.github.com>
2024-09-24 19:11:29 -0700

    Add Text for the MainScreen that will be responsible for showing data of a given item in a list.
    Add Preview of the MainScreen.

--- app/src/main/java/com/chiu/hiit_time/ui/main/MainScreen.kt
+++ app/src/main/java/com/chiu/hiit_time/ui/main/MainScreen.kt
@@ -1,11 +1,22 @@
 package com.chiu.hiit_time.ui.main
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.chiu.hiit_time.ui.theme.HIITTIMETheme
 
 @Composable
 fun MainScreen(
     modifier: Modifier
 ) {
+    Text(text = "Genshin Character Info Screen")
+}
 
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    HIITTIMETheme {
+        MainScreen(modifier = Modifier)
+    }
 }
