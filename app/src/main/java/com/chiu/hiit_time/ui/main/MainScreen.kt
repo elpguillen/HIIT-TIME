1c835e1817bcf6e7377bafe7d7b925dce4a2c3b3
elpguillen <elpguillen@users.noreply.github.com>
2024-09-26 19:19:36 -0700

    Modify MainScreen layout to include Title, Character Name, Character Element, and Character Region.

--- app/src/main/java/com/chiu/hiit_time/ui/main/MainScreen.kt
+++ app/src/main/java/com/chiu/hiit_time/ui/main/MainScreen.kt
@@ -1,16 +1,28 @@
 package com.chiu.hiit_time.ui.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chiu.hiit_time.ui.theme.HIITTIMETheme
 
 @Composable
 fun MainScreen(
     modifier: Modifier
 ) {
-    Text(text = "Genshin Character Info Screen")
+     Column(
+         modifier = Modifier
+             .padding(horizontal = 8.dp)
+     ) {
+        Text(text = "Genshin Character Info Screen", modifier = Modifier.padding(vertical = 8.dp))
+        Text(text = "Character Name", modifier = Modifier.padding(vertical = 8.dp))
+        Text(text = "Character Element", modifier = Modifier.padding(vertical = 8.dp))
+        Text(text = "Character Region", modifier = Modifier.padding(vertical = 8.dp))
+    }
 }
 
 @Preview(showBackground = true)
